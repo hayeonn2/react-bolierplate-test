f2968547b3de152cdcf8581ddf726d0e910dd69a
hayoenn2 <dwd1205@naver.com>
2023-11-03 00:36:20 +0900

    feat :: plugin, rules 적용 - HtmlWebPackPlugin - MiniCssExtractPlugin

---
+++ webpack.config.js
@@ -0,0 +1,66 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const mode = process.env.NODE_ENV || 'development'; // 기본값 development
+
+module.exports = () => ({
+  mode,
+  module: {
+    rules: [
+      {
+        test: /\.(tsx|ts|js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.(svg|png|jpg|jpeg|gif|ico)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[path][name].[ext]',
+        },
+      },
+      {
+        test: /\.(css)$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'],
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader', // html-loader 웹팩이 이해할 수 있게 하고 options 로는 minimize라는 코드 최적화 옵션을 사용하고 있다.
+            options: { minimize: true },
+          },
+        ],
+      },
+    ],
+  },
+  // 플러그인 설정
+  plugins: [
+    // HtmlWebPackPlugin 웹팩이 html 파일을 읽어서 html 파일을 빌드할 수 있게 해준다.
+    new HtmlWebPackPlugin({
+      template: './public/index.html',
+      filename: 'index.html',
+      favicon: './public/favicon.ico',
+      minify: process.env.NODE_ENV === 'production' && {
+        collapseWhitespace: true, // 빈칸 제거
+        removeComments: true, // 주석제거
+      },
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+    }),
+    // 환경 변수 등록/관리 설정
+    new webpack.EnvironmentPlugin({
+      NODE_ENV: 'development',
+    }),
+  ],
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+    alias: {
+      '@components': path.resolve(__dirname, 'src/components/'),
+      // '@pages': path.resolve(__dirname, 'src/pages/'),
+    },
+  },
+});
