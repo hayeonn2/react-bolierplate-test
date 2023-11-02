4c964741a7979a61f7f0ce9e94b8f51e173cac04
hayoenn2 <dwd1205@naver.com>
2023-11-03 00:33:54 +0900

    feat :: components 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import '../src/tailwind.css';
+import Example from '@components/Example';
+import ExampleTwo from '@components/ExampleTwo';
+import { Title } from '@components/Title';
+import { Button } from './components/Button';
 
 function App() {
-  return <div className="App">리액트 보일러플레이트</div>;
+  return (
+    <div className="App">
+      리액트 보일러플레이트
+      <Title size="2xl">asdasd</Title>
+      <Button text="제목" />
+      <Example value="벨류" />
+      <ExampleTwo />
+      <p>{process.env.NODE_ENV}</p>
+    </div>
+  );
 }
 
 export default App;
